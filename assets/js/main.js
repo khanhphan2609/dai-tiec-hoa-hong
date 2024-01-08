d7848e8aa4a3ce3684a904a39c694549b8f1bdec
khanhphan2609 <103134275+khanhphan2609@users.noreply.github.com>
2024-01-08 16:26:50 +0700

    fix 8/1

---
+++ assets/js/main.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+    // The le
+    $(document).on('click', '.modal-guide', function() {
+        DialogGuide();
+    });
+    // History
+    $('.card-routes').on('click', '.history', function() {
+        AskToLogIn();
+    });
+    // BXH
+    $(document).on('click', '.charts-btn', function() {
+        DialogCharts();
+    });
+    // Doi qua
+    var targetOffset = $('.get-gifts').offset().top;
+    $('.card-routes').on('click', '.scroll-to-gift', function () {
+        ScrollToGift(targetOffset);
+    });
+    // Them luot
+    $('.more-turns').on('click', '.btn', function () {
+        AskToLogIn();
+    });
+    // Get gifts
+    $('.get-gifts').on('click', 'a', function () {
+        AskToLogIn();
+    });
+
+    // Footer Mobile
+    $('.routes-mob').on('click', '.modal-guide', function() {
+        DialogGuide();
+    });
+    $(document).on('click', '.history', function() {
+        AskToLogIn();
+    });
+    $(document).on('click', '.scroll-to-gift', function() {
+        ScrollToGift(targetOffset);
+    });
+});
+
+// Modal
+function DialogGuide() {
+    $('#dialogGuide').modal('toggle');
+};
+
+function DialogCharts() {
+    $('#dialogCharts').modal('toggle');
+};
+
+// Scroll
+function ScrollToGift(targetOffset) {
+    $('html, body').animate({
+        scrollTop: targetOffset
+    }, 100);
+}
+
+// SweetAlert2
+function AskToLogIn() {
+    Swal.fire("Vui lòng đăng nhập!");
+}
+
